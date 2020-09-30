8d93377071674d61682dccaf23fbfdaaf92c4297
dhmec <dharmoradiya@gmail.com>
2020-09-30 13:25:20 -0600

    add echo in title and h1 in index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$pageTitle = 'TechCareers | Basic PHP Webpage';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <!--Script(s)-->
+    <script type="text/JavaScript" src="js/scripts.js" defer></script>
+</head>
+<body>
+    <h1><?php echo $pageTitle; ?></h1>
+</body>
+</html>
