1e98c1e17862ce536b556954a9234635dcbfd5e2
dhmec <dharmoradiya@gmail.com>
2020-09-30 14:17:17 -0600

    add nav links

--- about.php
+++ about.php
@@ -15,7 +15,6 @@ $pageTitle = 'TechCareers | Basic PHP Webpage';
     <script type="text/JavaScript" src="js/scripts.js" defer></script>
 </head>
 <body>
-    <h1><?php echo $pageTitle; ?></h1>
-    <?php include './includes/navigation.php';?>
+    <h2>Hi, This is about page!</h2>
 </body>
 </html>
